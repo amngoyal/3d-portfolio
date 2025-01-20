88dd69f057a32d4a05915aa3b875558c8a739839
Aman Goyal <aman.goyal@qed42.com>
2025-01-21 00:19:32 +0530

    Create hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative mx-auto w-full h-screen">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="bg-[#915eff] rounded-full w-5 h-5" />
+          <div className="w-1 h-40 sm:h-80 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            {"Hi, I'm "}
+            <span className="text-[#915eff]">Aman</span>
+          </h1>
+          <p className={`${styles.heroSubText} text-white-100`}>
+            {"I'm a full stack developer with a "}
+            <br className="sm:block hidden" /> passion for creating web
+            applications.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
 
-export default Hero
+      <div className="bottom-0 absolute flex justify-center items-center w-full">
+        <a href="#about">
+          <div className="flex justify-center items-start border-4 border-secondary p-2 rounded-3xl w-[35px] h-[64px]">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+              className="bg-secondary mb-1 rounded-full w-3 h-3"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
-
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+import { styles } from '../styles';
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
 
-export default Navbar
+  useEffect(() => {
+    return () => {};
+  }, []);
+
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="flex justify-between items-center mx-auto w-full max-w-7xl">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive('');
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="flex font-bold text-[18px] text-white cursor-pointer">
+            Aman &nbsp;
+            <span className="sm:block hidden">| Frontend Developer</span>
+          </p>
+        </Link>
+        <ul className="sm:flex flex-row gap-10 hidden list-none">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? 'text-white' : 'text-secondary'
+              } hover:text-secondary text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex flex-1 justify-end items-center sm:hidden">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] cursor-pointer object-contain"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${
+              !toggle ? 'hidden' : 'flex'
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="flex flex-col justify-end items-start gap-4 list-none">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? 'text-white' : 'text-secondary'
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
